8b6e54525d7b98342daac117debe5f8c41a605a3
nailtonsj <nailtonsj.2000@gmail.com>
2023-04-10 19:07:14 -0300

    adicionado animacao ao scroll

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ setInterval(() => {
   typeWriter(elemento);
 }, 10000);
 
-const ativarUl = document.querySelectorAll(".formacao-extra h3");
+const ativarUl = document.querySelectorAll(".js .formacao-extra h3");
 if (ativarUl) {
   ativarUl.forEach((item, index) => {
     item.addEventListener("click", function (e) {
@@ -22,3 +22,30 @@ if (ativarUl) {
     });
   });
 }
+const target = document.querySelectorAll(".js [data-anime]");
+const animationClass = "animate";
+// function animeScroll() {
+//   target.forEach(function (element) {
+//     if (windowMetade > element.offsetTop) {
+//       element.classList.add(animationClass);
+//     }
+//     element.offsetTop;
+//   });
+// }
+
+// window.addEventListener("scroll", function () {
+//   animeScroll();
+// });
+const windowMetade = window.innerHeight * 0.6;
+addEventListener("scroll", function () {
+  {
+    target.forEach((item, index) => {
+      const altura = item.getBoundingClientRect().top;
+      if (altura - windowMetade <= 0) {
+        item.classList.add("animate");
+      } else {
+        item.classList.remove("animate");
+      }
+    });
+  }
+});
